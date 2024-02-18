4bea0833622a0f2c8a63c442df016f2eb1f81591
kei1_st <kei1satoh0603@gmail.com>
2024-02-18 22:56:08 +0900

    about ページへのリンクを追加

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ layout.js -> Define wrapper arond pages
 not-found.js -> Define "Not Found" fallback page
 
 error.js -> Define "Error" fallback page
+
+# ページの追加
+- 手打ち URL のアクセスはサーバーサイドのJSを呼び出す
+- リンク経由のアクセスはクライアントサイドのみで完結する
+  - 最初のアクセス時はサーバーサイドレンダリングが必要
--- app/page.js
+++ app/page.js
@@ -1,6 +1,8 @@
 // page.js は Next.js の予約ファイル名 index.html 的な
 // Nextjs 側の処理によってサーバー上で実行される
 
+import Link from 'next/link';
+
 export default function Home() {
   // console.log('Executing'); // reload page または rebuild するたびターミナルに出力される
   return (
@@ -8,6 +10,8 @@ export default function Home() {
       <img src="/logo.png" alt="A server surrounded by magic sparkles." />
       <h1>Welcome to this NextJS Course!</h1>
       <p>🔥 Let&apos;s get started! 🔥</p>
+      {/*<p><a href="/about">About Us</a></p> */} {/* anchor タグを使うとサーバーアクセスが発生し SPA のメリットが失われる */}
+      <p><Link href="/about">About Us</Link></p>  {/* Link を使うと新しいページのロードを回避できる */}
     </main>
   );
 }
