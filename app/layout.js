269a359493246c85594c2a905352b329be626204
kei1_st <kei1satoh0603@gmail.com>
2024-02-18 23:02:05 +0900

    add some comments

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,12 @@
 import './globals.css'
 
-export const metadata = {
+export const metadata = { // 予約変数, <head> で記述する内容は metadeta に記述
   title: 'NextJS Course App',
   description: 'Your first NextJS app!',
 };
 
-export default function RootLayout({ children }) {
+// layout は複数のコンポーネントの wrapper になることができる
+export default function RootLayout({ children }) {  // children: 現在アクティブなページのコンテンツ
   return (
     <html lang="en">
       <body>{children}</body>
