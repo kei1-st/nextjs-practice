0ed24da95b0d56602889b5e9720e8691d54e8337
kei1_st <kei1satoh0603@gmail.com>
2024-02-18 23:12:19 +0900

    add Header

--- app/page.js
+++ app/page.js
@@ -3,12 +3,13 @@
 
 import Link from 'next/link';
 
+import Header from '@/components/header'; // @ はフォルダのルートパスを表す
+
 export default function Home() {
   // console.log('Executing'); // reload page または rebuild するたびターミナルに出力される
   return (
     <main>
-      <img src="/logo.png" alt="A server surrounded by magic sparkles." />
-      <h1>Welcome to this NextJS Course!</h1>
+      <Header />
       <p>🔥 Let&apos;s get started! 🔥</p>
       {/*<p><a href="/about">About Us</a></p> */} {/* anchor タグを使うとサーバーアクセスが発生し SPA のメリットが失われる */}
       <p><Link href="/about">About Us</Link></p>  {/* Link を使うと新しいページのロードを回避できる */}
