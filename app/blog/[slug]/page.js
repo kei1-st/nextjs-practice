f9c74cc235e75c54ee39fc9e497f54900125cef1
kei1_st <kei1satoh0603@gmail.com>
2024-02-19 21:13:37 +0900

    add comments

--- app/blog/[slug]/page.js
+++ app/blog/[slug]/page.js
@@ -1,7 +1,8 @@
-export default function BlogPostPage() {
+export default function BlogPostPage({params}) {    // params は nextjs の仕様
   return (
     <main>
       <h1>Blog Post</h1>
+      <p>{params.slug}</p>  {/* ページに対して一意 */}
     </main>
   );
 }
